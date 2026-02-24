7013a0fe4ce184a0c0dd20b76075aaf9c9b08f1c
zilzarrt24-eng <zil.zar.rt24@dypatil.edu>
2026-02-24 17:58:47 +0530

    Created MainNavigation with BottomNavigationBar

---
+++ frontend-app/disaster_app/lib/screens/main_navigation.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'home_screen.dart';
+import 'disaster_list_screen.dart';
+import 'settings_screen.dart';
+
+class MainNavigation extends StatefulWidget {
+  const MainNavigation({super.key});
+
+  @override
+  State<MainNavigation> createState() => _MainNavigationState();
+}
+
+class _MainNavigationState extends State<MainNavigation> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = const [
+    HomeScreen(),
+    DisasterListScreen(),
+    SettingsScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.warning),
+            label: "Disasters",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "Settings",
+          ),
+        ],
+      ),
+    );
+  }
+}
