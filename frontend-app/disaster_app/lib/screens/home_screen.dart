6ad81930c8b949c824a7bacdd49bf551c566ee5b
zilzarrt24-eng <zil.zar.rt24@dypatil.edu>
2026-02-24 17:30:30 +0530

    Upgraded HomeScreen with dashboard layout and integrated SOSButton

--- frontend-app/disaster_app/lib/screens/home_screen.dart
+++ frontend-app/disaster_app/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/sos_button.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,10 +8,41 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Disaster Management')),
-      body: const Center(
-        child: Text(
-          'Offline-First Disaster Alert System',
-          style: TextStyle(fontSize: 18),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.green[100],
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: const Text(
+                "Status: SAFE",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.green,
+                ),
+              ),
+            ),
+            const SizedBox(height: 30),
+            SOSButton(
+              onPressed: () {
+                ScaffoldMessenger.of(
+                  context,
+                ).showSnackBar(const SnackBar(content: Text("SOS Triggered")));
+              },
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("View Nearby Disasters"),
+            ),
+          ],
         ),
       ),
     );
